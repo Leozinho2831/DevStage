0309838d30489e6af2bee91f4a3bdde1aa400dba
Leozinho2831 <leoferraz1818@gmail.com>
2025-02-27 15:34:10 -0300

    new code to started page

--- src/script.js
+++ src/script.js
@@ -90,32 +90,62 @@ const formAction = () => {
 
 const startApp = () => {
     const content = `
-    <form action="#" id="form" class="space-y-3">
-        <div class="relative">
-            <img class="absolute top-[18px] left-4 w-5 h-5" src="src/images/icons/email.svg" alt="E-mail">
-            <input 
-                class="input" 
-                type="email"
-                name="email"
-                placeholder="E-mail" 
-                required
-            >
-        </div>
-        <div class="relative">
-            <img class="absolute top-[18px] left-4 w-5 h-5" src="src/images/icons/phone.svg" alt="Telefone">
-            <input 
-                class="input" 
-                type="tel"
-                name="phone"
-                placeholder="Telefone"
-                required
-            >
-        </div>
-        <button class="btn !mt-6 flex justify-between gap-4" type="submit">
-            Confirmar
-            <img class="w-6 h-6" src="src/images/icons/arrow-right.svg" alt="seta para direita">
-        </button>
-    </form>`;
+        <section class="max-w-96 w-full">
+            <header class="grid gap-8">
+                <figure class="grid place-items-center">
+                    <img id="logo" class="w-28 h-8" src="src/images/logo.svg" alt="devStage">
+                </figure>
+                <h1 class="text-oxa-lg font-oxanium text-gray-100 text-center">
+                    <span class="block text-blue">CodeCraft</span>
+                    Summit 2025
+                </h1>
+            </header>
+            <section class="mt-12 space-y-5">
+                <div class="bg-gray-700 p-6 rounded-2xl">
+                    <header class="flex justify-between gap-6">
+                        <h2 class="text-oxa-sm font-oxanium text-gray-200">Sobre o evento</h2>
+                        <figure class="flex gap-1">
+                            <img src="src/images/icons/radio.svg" alt="Ao vivo">
+                            <span class="text-purple text-mont-xs uppercase">Ao vivo</span>
+                        </figure>
+                    </header>
+                    <div class="text-mont-sm text-gray-300 mt-6">
+                        <p>
+                            Um evento feito por e para pessoas desenvolvedoras apaixonadas por criar soluções inovadoras e compartilhar conhecimento. Vamos mergulhar nas tendências mais recentes em desenvolvimento de software, arquitetura de sistemas e tecnologias emergentes, com palestras, workshops e hackathons.
+                        </p>
+                        <span class="block mt-6">Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito</span>
+                    </div>
+                </div>
+                <form action="#" id="form" class="space-y-3 bg-gray-700 p-6 rounded-2xl">
+                    <h3 class="text-gray-200 text-oxa-sm font-oxanium">Inscrição</h3>
+                    <div class="relative !mt-6">
+                        <img class="absolute top-[18px] left-4 w-5 h-5" src="src/images/icons/email.svg" alt="E-mail">
+                        <input 
+                            class="input" 
+                            type="email"
+                            name="email"
+                            placeholder="E-mail" 
+                            required
+                        >
+                    </div>
+                    <div class="relative">
+                        <img class="absolute top-[18px] left-4 w-5 h-5" src="src/images/icons/phone.svg" alt="Telefone">
+                        <input 
+                            class="input" 
+                            type="tel"
+                            name="phone"
+                            placeholder="Telefone"
+                            required
+                        >
+                    </div>
+                    <button class="btn !mt-6 flex justify-between gap-4" type="submit">
+                        Confirmar
+                        <span class="icon-arrow"></span>
+                    </button>
+                </form>
+            </section>
+        </section>
+    `;
     app.innerHTML = content;
 
     formAction();
@@ -123,4 +153,4 @@ const startApp = () => {
 
 startApp();
 
-// document.querySelector("#logo").onclick = () => startApp();
+document.querySelector("#logo").onclick = () => startApp();
